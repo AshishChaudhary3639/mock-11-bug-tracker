f81dae666f419fd48c8681034b7e82d4b29c0c77
AshishChaudhary3639 <ashishchaudhary3639@gmail>
2023-01-19 14:35:09 +0530

    home route changes

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,9 @@ const app=express()
 app.use(express.json())
 app.use(cors())
 
+app.use("/",(req,res)=>{
+    res.send("Welcome")
+})
 app.use("/",userRoute)
 app.listen(process.env.PORT,async()=>{
 
